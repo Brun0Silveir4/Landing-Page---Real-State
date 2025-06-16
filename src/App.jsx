d43250202bb8edaeb3a8512188500122d1922a0e
Brun0Silveir4 <bruno.dionisio@fatec.sp.gov.br>
2025-06-16 16:00:09 -0300

    add: starting recent projets section

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,10 @@ import "./App.scss";
 import Header from "./components/header/Header";
 import House1 from "./components/house1/House1";
 import chat from "../public/images/message.svg";
-import email from "../public/images/email.svg"
-import pen from "../public/images/pen.svg"
+import email from "../public/images/email.svg";
+import pen from "../public/images/pen.svg";
 import CardInfo from "./components/card-infos/CardInfo";
+import { MdOutlineArrowBackIos } from "react-icons/md";
 
 function App() {
   return (
@@ -26,19 +27,47 @@ function App() {
             <CardInfo
               img={chat}
               title={"Answer Questions"}
-              text={"Lorem ipsum dolor sit amet consectetur. Adipiscing imperdietbibendum in in vestibulum"}/>
-            
+              text={
+                "Lorem ipsum dolor sit amet consectetur. Adipiscing imperdietbibendum in in vestibulum"
+              }
+            />
+
             <CardInfo
               img={email}
               title={"Select a quote"}
-              text={"Lorem ipsum dolor sit amet consectetur. Adipiscing imperdietbibendum in in vestibulum"}/>
+              text={
+                "Lorem ipsum dolor sit amet consectetur. Adipiscing imperdietbibendum in in vestibulum"
+              }
+            />
 
             <CardInfo
               img={pen}
               title={"Get registered"}
-              text={"Lorem ipsum dolor sit amet consectetur. Adipiscing imperdietbibendum in in vestibulum"}/>              
+              text={
+                "Lorem ipsum dolor sit amet consectetur. Adipiscing imperdietbibendum in in vestibulum"
+              }
+            />
           </div>
 
+          <div className="recents">
+            <div className="info-recent">
+              <p>Best Project of the Years</p>
+            </div>
+
+            <div className="title-recent">
+              <p>Our recent projects</p>
+              <div className="arrows-group">
+                <div className="arrow">
+                  <MdOutlineArrowBackIos size={24} />
+                </div>
+                <div className="arrow reverse">
+                  <MdOutlineArrowBackIos size={24} color="white"/>
+                </div>
+              </div>
+            </div>
+
+            <div className="projects-items"></div>
+          </div>
         </div>
       </div>
     </div>
